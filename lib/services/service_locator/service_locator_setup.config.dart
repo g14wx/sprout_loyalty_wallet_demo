8481539ce623cb27a8f5454f017aace8842f723d
g14wxz <carlos.hernandez3@catolica.edu.sv>
2023-12-13 15:49:37 -0600

    feat(02): add store three run flavorizr

--- lib/services/service_locator/service_locator_setup.config.dart
+++ lib/services/service_locator/service_locator_setup.config.dart
@@ -12,21 +12,27 @@ import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
 import '../../features/root/presentation/BLoC/fetch_store_bloc/fetch_store_bloc.dart' as _i3;
-import '../../global/providers/app_settings_provider/app_settings_provider.dart' as _i15;
+import '../../global/providers/app_settings_provider/app_settings_provider.dart' as _i17;
 import '../../global/providers/app_settings_provider/app_settings_storage_manager/implementations/app_settings_storage_manager.dart'
-    as _i14;
+    as _i16;
 import '../../global/providers/app_settings_provider/app_settings_storage_manager/protocols/i_app_settings_storage_manager.dart'
-    as _i13;
-import '../../router/providers/navigator_keys_provider.dart' as _i12;
+    as _i15;
+import '../../router/providers/navigator_keys_provider.dart' as _i14;
 import '../../themes/protocols/i_color_schema.dart' as _i4;
-import '../../themes/protocols/i_custom_colors_container.dart' as _i6;
-import '../../themes/storeone/color_schemes.g.storeone.dart' as _i5;
-import '../../themes/storeone/custom_color.g.storeone.dart' as _i7;
-import '../data_storage/implementations/data_storage.dart' as _i9;
-import '../data_storage/implementations/initialization_of_data_storage_service.dart' as _i11;
-import '../data_storage/protocols/i_data_storage.dart' as _i8;
-import '../data_storage/protocols/i_initialization_of_data_storage_service.dart' as _i10;
+import '../../themes/protocols/i_custom_colors_container.dart' as _i7;
+import '../../themes/storeone/color_schemes.g.storeone.dart' as _i6;
+import '../../themes/storeone/custom_color.g.storeone.dart' as _i9;
+import '../../themes/storethree/color_schemes.g.storeone.dart' as _i5;
+import '../../themes/storethree/custom_color.g.storeone.dart' as _i8;
+import '../data_storage/implementations/data_storage.dart' as _i11;
+import '../data_storage/implementations/initialization_of_data_storage_service.dart' as _i13;
+import '../data_storage/protocols/i_data_storage.dart' as _i10;
+import '../data_storage/protocols/i_initialization_of_data_storage_service.dart' as _i12;
 
+const String _production_store_three = 'production_store_three';
+const String _staging_store_three = 'staging_store_three';
+const String _local_store_three = 'local_store_three';
+const String _test_store_three = 'test_store_three';
 const String _production_store_one = 'production_store_one';
 const String _staging_store_one = 'staging_store_one';
 const String _local_store_one = 'local_store_one';
@@ -45,7 +51,16 @@ _i1.GetIt $initGetIt(
   );
   gh.factory<_i3.FetchStoreBloc>(() => _i3.FetchStoreBloc());
   gh.factory<_i4.IColorSchema>(
-    () => _i5.ColorSchemaStoreOne(),
+    () => _i5.ColorSchemaStoreThree(),
+    registerFor: {
+      _production_store_three,
+      _staging_store_three,
+      _local_store_three,
+      _test_store_three,
+    },
+  );
+  gh.factory<_i4.IColorSchema>(
+    () => _i6.ColorSchemaStoreOne(),
     registerFor: {
       _production_store_one,
       _staging_store_one,
@@ -53,8 +68,17 @@ _i1.GetIt $initGetIt(
       _test_store_one,
     },
   );
-  gh.factory<_i6.ICustomColorsContainer>(
-    () => _i7.CustomColorsContainerStoreOne(),
+  gh.factory<_i7.ICustomColorsContainer>(
+    () => _i8.CustomColorsContainerStoreThree(),
+    registerFor: {
+      _production_store_three,
+      _staging_store_three,
+      _local_store_three,
+      _test_store_three,
+    },
+  );
+  gh.factory<_i7.ICustomColorsContainer>(
+    () => _i9.CustomColorsContainerStoreOne(),
     registerFor: {
       _production_store_one,
       _staging_store_one,
@@ -62,12 +86,12 @@ _i1.GetIt $initGetIt(
       _test_store_one,
     },
   );
-  gh.factory<_i8.IDataStorage>(() => _i9.DataStorage());
-  gh.factory<_i10.IInitializationOfDataStorageService<dynamic>>(() => _i11.InitializationOfDataStorageService());
-  gh.factory<_i12.NavigatorKeysProvider>(() => _i12.NavigatorKeysProvider());
-  gh.factory<_i13.IAppSettingsStorageManager>(
-      () => _i14.AppSettingsStorageManager(dataStorage: gh<_i8.IDataStorage>()));
-  gh.factory<_i15.AppSettingsProvider>(
-      () => _i15.AppSettingsProvider(appSettingsManager: gh<_i13.IAppSettingsStorageManager>()));
+  gh.factory<_i10.IDataStorage>(() => _i11.DataStorage());
+  gh.factory<_i12.IInitializationOfDataStorageService<dynamic>>(() => _i13.InitializationOfDataStorageService());
+  gh.factory<_i14.NavigatorKeysProvider>(() => _i14.NavigatorKeysProvider());
+  gh.factory<_i15.IAppSettingsStorageManager>(
+      () => _i16.AppSettingsStorageManager(dataStorage: gh<_i10.IDataStorage>()));
+  gh.factory<_i17.AppSettingsProvider>(
+      () => _i17.AppSettingsProvider(appSettingsManager: gh<_i15.IAppSettingsStorageManager>()));
   return getIt;
 }
